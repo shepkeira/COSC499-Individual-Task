b636f5a5ec8dacb4aeef01639357c77ea70cae4a
shepkeira <shepherdkeira@gmail.com>
2021-09-15 19:17:37 -0700

    Adding testing

--- number_guesser.py
+++ number_guesser.py
@@ -6,19 +6,29 @@ def pick_number():
     high = 100
     low = 0
     guess = math.floor((high-low)/2 + low)
-    while True:
+    done = False
+    while not done:
         print("Is the number ", guess)
         ans = input("A - Correct!\nB - Too High\nC - Too Low\n")
-        if ans == "A":
-            print("I Won!")
-            break
-        elif ans == "B":
-            high = guess
-            guess = math.floor((high - low)/2 + low)
-        elif ans == "C":
-            low = guess
-            guess = math.floor((high - low)/2 + low)
-        else:
-            print("That wasn't an answer, please enter A, B, or C")
+        guess, done = change_guess(ans, guess, high, low)
+    return guess
 
-pick_number()
+
+
+def change_guess(ans, guess, high, low):
+    success = False
+    if ans == "A":
+        print("I Won!")
+        success = True
+    elif ans == "B":
+        high = guess
+        guess = math.floor((high - low)/2 + low)
+    elif ans == "C":
+        low = guess
+        guess = math.floor((high - low)/2 + low)
+    else:
+        print("That wasn't an answer, please enter A, B, or C")
+    return guess, success
+
+if __name__ == "__main__":
+    pick_number()
